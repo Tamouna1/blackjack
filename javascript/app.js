12e11618f782f62b9a2e2d0b1303b7ba28143203
Tamuna Janezashvili <95763995+Tamouna1@users.noreply.github.com>
2022-05-17 13:12:55 +0400

    update app.js

--- javascript/app.js
+++ javascript/app.js
@@ -1,40 +1,45 @@
-let firstCard = Math.floor(Math.random() * 11 ) + 2;
-let secondCard = Math.floor(Math.random() * 11 ) + 2;
-let cards = [firstCard,secondCard];
-let sum = firstCard + secondCard;
-console.log(sum);
+let firstCard = 10
+let secondCard = 4
+let cards = [firstCard, secondCard]
+let sum = firstCard + secondCard
+let hasBlackJack = false
+let isAlive = true
+let message = ""
+let messageEl = document.getElementById("message-el")
+let sumEl = document.getElementById("sum-el")
+let cardsEl = document.getElementById("cards-el")
 
-let hasBlackjack = false;
-let isAlive = true;
-let message = "";
-
-let sumEl = document.getElementById("sum-el");
-let cardsEl = document.getElementById("cards-el");
-
-let messageEL = document.getElementById("message-el");
-function startGame(){
-    renderGame();
+function startGame() {
+    renderGame()
 }
-function renderGame(){
-    cardsEl.textContent = "Cards: " + cards[0] + " " + cards[1];
-    sumEl.textContent = "Sum is: " + sum;
+
+function renderGame() {
+    cardsEl.textContent = "Cards: "
+    for (let i = 0; i < cards.length; i++) {
+        cardsEl.textContent += cards[i] + " "
+    }
+    
+    sumEl.textContent = "Sum: " + sum
     if (sum <= 20) {
-        message = "Do you want to draw a new card?";
-    }else if(sum === 21){
+        message = "Do you want to draw a new card?"
+    } else if (sum === 21) {
         message = "You've got Blackjack!"
-        hasBlackjack = true;
-    }else {
-        message = "You are out of the game! :( ";
-        isAlive = false;
-    };
-    messageEL.textContent = message;
+        hasBlackJack = true
+    } else {
+        message = "You're out of the game!"
+        isAlive = false
+    }
+    messageEl.textContent = message
 }
-function newCard(){
-    let card = Math.floor(Math.random() * 11 ) + 2;
-    sum += card;
-    cards.push(newCard);
-    renderGame();
+
+function newCard() {
+    let card = 6
+    sum += card
+    cards.push(card)
+    console.log(cards)
+    renderGame()
 }
+
 
 
 
