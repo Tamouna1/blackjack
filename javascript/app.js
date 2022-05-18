6a26933f55d5b8c0310179986fe8b181d227e5b3
Tamuna Janezashvili <95763995+Tamouna1@users.noreply.github.com>
2022-05-18 22:55:49 +0400

    app.js

--- javascript/app.js
+++ javascript/app.js
@@ -1,24 +1,22 @@
-let firstCard = 10
-let secondCard = 4
-let cards = [firstCard, secondCard]
-let sum = firstCard + secondCard
-let hasBlackJack = false
-let isAlive = true
-let message = ""
-let messageEl = document.getElementById("message-el")
-let sumEl = document.getElementById("sum-el")
-let cardsEl = document.getElementById("cards-el")
+let firstCard = getRandomCard();
+let secondCard = getRandomCard();
+let cards = [firstCard, secondCard];
+let sum = firstCard + secondCard;
+let hasBlackJack = false;
+let isAlive = true;
+let message = "";
+let messageEl = document.getElementById("message-el");
+let sumEl = document.getElementById("sum-el");
+let cardsEl = document.getElementById("cards-el");
 
 function startGame() {
-    renderGame()
+    renderGame();
 }
-
 function renderGame() {
     cardsEl.textContent = "Cards: "
     for (let i = 0; i < cards.length; i++) {
         cardsEl.textContent += cards[i] + " "
     }
-    
     sumEl.textContent = "Sum: " + sum
     if (sum <= 20) {
         message = "Do you want to draw a new card?"
@@ -29,15 +27,14 @@ function renderGame() {
         message = "You're out of the game!"
         isAlive = false
     }
-    messageEl.textContent = message
+    messageEl.textContent = message;
 }
-
 function newCard() {
-    let card = 6
-    sum += card
-    cards.push(card)
-    console.log(cards)
-    renderGame()
+    let card = getRandomCard();
+    sum += card;
+    cards.push(card);
+    console.log(cards);
+    renderGame();
 }
 
 
